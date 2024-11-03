44e706a7d1649f281e888682473a50d1f4e1ce22
hetvi3012 <157871396+hetvi3012@users.noreply.github.com>
2024-11-03 17:56:42 +0530

    Add files via upload

---
+++ include/FareCalculator.h
@@ -0,0 +1,9 @@
+// include/FareCalculator.h
+#ifndef FARECALCULATOR_H
+#define FARECALCULATOR_H
+
+double calculateDistance(double lat1, double lon1, double lat2, double lon2);
+double calculateFare(double distance);
+
+#endif // FARECALCULATOR_H
+
